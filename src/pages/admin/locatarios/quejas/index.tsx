5e8cae00cce2c33fd2eb9678872232131360113b
BrunoDiaz7 <brunoleandrodiaz07@gmail.com>
2024-04-26 08:53:43 -0300

    agregar skeleton screen

--- src/pages/admin/locatarios/quejas/index.tsx
+++ src/pages/admin/locatarios/quejas/index.tsx
@@ -2,6 +2,8 @@ import {MainAppLayout} from '@/layouts';
 import {ReportAdminCard} from '@/components';
 import {getAllReports} from '@/services/reports';
 import {useEffect, useState} from 'react';
+import {SkeletonScreen} from '@/components';
+import {Box, Typography} from '@mui/material';
 
 const UserReports = () => {
   const [reports, setReports] = useState([]);
@@ -21,24 +23,32 @@ const UserReports = () => {
   useEffect(() => {
     obtenerReportes();
   }, []);
+
   return (
     <MainAppLayout>
-      {reports.length > 0
-        ? reports.map((reporte: Object) => (
-            <ReportAdminCard
-              // @ts-ignore
-              key={reporte}
-              // @ts-ignore
-              id_queja={reporte.id_queja}
-              // @ts-ignore
-              tipo={reporte.tipo}
-              // @ts-ignore
-              estado={reporte.estado}
-              // @ts-ignore
-              descripcion={reporte.descripcion}
-            />
-          ))
-        : ''}
+      {reports.length > 0 ? (
+        reports.map((reporte: Object) => (
+          <ReportAdminCard
+            // @ts-ignore
+            key={reporte}
+            // @ts-ignore
+            id_queja={reporte.id_queja}
+            // @ts-ignore
+            tipo={reporte.tipo}
+            // @ts-ignore
+            estado={reporte.estado}
+            // @ts-ignore
+            descripcion={reporte.descripcion}
+          />
+        ))
+      ) : (
+        <Box sx={{display: 'flex', flexDirection:'column', width: '100%', alignItems: 'center', gap: '20px', marginBottom: '2%'}}>
+          <Typography variant="body2">...Cargando reportes</Typography>
+          <SkeletonScreen sx={{height: '200px'}} />
+          <SkeletonScreen sx={{height: '200px'}} />
+          <SkeletonScreen sx={{height: '200px'}} />
+        </Box>
+      )}
     </MainAppLayout>
   );
 };
